4d9b43ce55f01b384396032c5b59a8f332b72a06
leomaks <s.arkhin@gmail.com>
2023-05-02 14:49:23 +0200

    add Calc.java, update README.md

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/1fa32f5ab3ca3b158b4b/maintainability)](https://codeclimate.com/github/leomaks/java-project-61/maintainability)
 ### Игра: "Проверка на чётность"
 https://asciinema.org/a/6tg90B8DcdUZNb5L3H20eNoF8
+### Игра: "Калькулятор"
+https://asciinema.org/connect/8c1ec0ff-7186-48fa-82e4-eef8dcfc5705
+
---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,37 @@
+package hexlet.code.games;
+import hexlet.code.Engine;
+
+import java.util.Scanner;
+
+public class Calc {
+
+    public static void play() {
+        System.out.println("What is the result of the expression?");
+        Engine.play(3);
+    }
+    public static boolean playRound() {
+
+        int number1 =  (int) (Math.random() * 10);
+        int number2 =  (int) (Math.random() * 10);
+        int operation =  (int) (Math.random() * 10);
+        String operand = "";
+        int result = 0;
+        switch (operation % 3) {
+            case 0: operand = "+"; result = number1 + number2;
+                break;
+            case 1: operand = "-"; result = number1 - number2;
+                break;
+            case 2: operand = "*"; result = number1 * number2;
+                break;
+        }
+
+        boolean win = false;
+
+        System.out.println("Question: " + number1 + operand + number2);
+        var rightAnswer = Integer.toString(result);
+
+        win = Engine.isAnswerCorrect(rightAnswer);
+        return win;
+    }
+
+}
