b0b29268e52d5ffe72d820586f6271d6d2febdea
leomaks <s.arkhin@gmail.com>
2023-05-02 11:29:41 +0200

    add Cli.java update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
+import java.util.Scanner;
+
+
 
 public class App {
+    public static String name;
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+  //      System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+
+        // takes input from the keyboard
+        Scanner input = new Scanner(System.in);
+
+        // prints the choice
+        System.out.print("Your choice:  ");
+        String start = input.nextLine();
+
+        if (start.equals("1")) {
+            Cli.getUserName();
+        }
+
+      /*  if (start.equals("2")) {
+            Even.play();
+        }
+*/
     }
+
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+    public static void getUserName() {
+        // creates an object of Scanner
+        System.out.println("Welcome to the Brain Games!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+
+        // takes input from the keyboard
+        App.name = input.nextLine();
+
+        // prints the name
+        System.out.println("Hello, " + App.name + "!");
+
+    }
+
+}
