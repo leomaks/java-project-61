507f9ede61627f151566d02bda0c31314bdb6544
leomaks <s.arkhin@gmail.com>
2023-05-12 13:16:09 +0200

    update methods in Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,17 +5,27 @@ public class Even {
     public static final int MAX = 1000;
     public static void play() {
 
-        String[][] roundsData = new String[Engine.ROUNDS_COUNT][2];
+        String[][] roundsData = new String[Engine.ROUNDS_COUNT][];
         String description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-        for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
-
-            int number =  (int) (Math.random() * MAX);
-            roundsData[i][0] = String.valueOf(number); // question
-            roundsData[i][1] = (number % 2 == 0) ? "yes" : "no"; // right answer
-        }
+        for (int i = 0; i < Engine.ROUNDS_COUNT; i++)
+            roundsData[i] = generateRoundData();
 
         Engine.play(description, roundsData);
+    }
+
+    public static String[] generateRoundData() {
+
+        String[] roundData = new String[2];
+        int number =  (int) (Math.random() * MAX);
+        roundData[0] = String.valueOf(number); // question
+        roundData[1] = isEven(number) ? "yes" : "no"; // right answer
+
+        return roundData;
+    }
+
+    public static boolean isEven(int x) {
+        return x % 2 == 0;
     }
 
 }
