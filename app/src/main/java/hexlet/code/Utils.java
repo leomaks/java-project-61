d6c9aef3b04f2c004ad1d88092e9ef45fab5b51d
leomaks <s.arkhin@gmail.com>
2023-05-12 15:45:10 +0200

    update utils

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,7 +1,4 @@
 package hexlet.code;
-
-import hexlet.code.Engine;
-
 public class Utils {
     public static int generateNumber(int rightBorder) {
         return (int) (Math.random() * rightBorder);
